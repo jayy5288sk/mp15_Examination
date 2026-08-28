4f951995893fd7da87198c8f9c862f2d59b29ba8
Jay <jayy5288@gmail.com>
2026-08-28 10:26:13 +0900

    [Feat] 가게 선정 및 ConsoleInput을 통한 범위 설정

--- 260828_Exam_IDE/260828_Exam_IDE/Program.cs
+++ 260828_Exam_IDE/260828_Exam_IDE/Program.cs
@@ -2,8 +2,12 @@
 
 class Program
 {
+    private const string SHOP_NAME = "버거킥";
+    
     static void Main(string[] args)
     {
+        int menuNumber = ConsoleInput.ReadIntInRange("메뉴 번호 : ", 1, 8);
+        int payLeast = ConsoleInput.ReadIntAtLeast("받은 금액 : ", 0);
         
     }
 }
